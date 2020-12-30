85a1fdc2f6edf7e6750b396fb47b1a097b092443
Sebastien Auray <sau@lem.com>
2020-12-30 09:22:14 +0100

    up

--- webpi/settings.py
+++ webpi/settings.py
@@ -123,4 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static_prod/")  # for production
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/"),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/"), ]
